86550b840c5dbf56af9316ebe45e16e112afd3bb
amir8602 <amirreza8602@gmail.com>
2022-04-08 19:35:08 +0430

    end 065

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,16 +2,10 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.Model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
+
+public interface OwnerService extends CrudService<Owner , Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner indById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -5,12 +5,6 @@ import guru.springframework.sfgpetclinic.Model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }
